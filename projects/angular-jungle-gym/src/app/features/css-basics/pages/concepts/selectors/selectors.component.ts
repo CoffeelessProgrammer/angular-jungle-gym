19d6e111cd241a6a1ba1abe83813a43f4cd77a7e
OmnicWolf <87935417+OmnicWolf@users.noreply.github.com>
2025-05-07 18:25:26 -0400

    Upgrade to Ng 19

--- projects/angular-jungle-gym/src/app/features/css-basics/pages/concepts/selectors/selectors.component.ts
+++ projects/angular-jungle-gym/src/app/features/css-basics/pages/concepts/selectors/selectors.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-selectors',
   templateUrl: './selectors.component.html',
-  styleUrls: ['./selectors.component.scss']
+  styleUrls: ['./selectors.component.scss'],
+  standalone: false
 })
 export class SelectorsComponent {
 
